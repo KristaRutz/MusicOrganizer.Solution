0c75e5acd633f48f0f5b34ad6efcb8dd8b8a8a16
Krista Rutz <rutzkri000@gmail.com>
2020-03-12 10:34:25 -0700

    write and pass tests for Artist class logic Co-authored-by: Steven Fleming <12flemings@gmail.com> Co-authored-by: Krista Rutz <rutzkri000@gmail.com> Co-authored-by: Jack Dunning <JackStunning9001@gmail.com>

--- MusicOrganizer.Tests/ModelTests/ArtistTests.cs
+++ MusicOrganizer.Tests/ModelTests/ArtistTests.cs
@@ -19,5 +19,66 @@ namespace MusicOrganizer.Tests
             Artist newArtist = new Artist("Blue Swede");
             Assert.AreEqual(typeof(Artist), newArtist.GetType());
         }
+
+        [TestMethod]
+        public void GetName_ReturnsName_String()
+        {
+            //Arrange
+            string name = "Amy Winehouse";
+            Artist newArtist = new Artist(name);
+
+            //Act
+            string result = newArtist.Name;
+
+            //Assert
+            Assert.AreEqual(name, result);
+        }
+
+        [TestMethod]
+        public void GetId_ReturnsArtistId_Int()
+        {
+            //Arrange
+            string name = "The Beatles";
+            Artist newArtist = new Artist(name);
+
+            //Act
+            int result = newArtist.Id;
+
+            //Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void GetAll_ReturnsAllArtistObjects_ArtistList()
+        {
+            //Arrange
+            string name01 = "Etta James";
+            string name02 = "Drake";
+            Artist newArtist1 = new Artist(name01);
+            Artist newArtist2 = new Artist(name02);
+            List<Artist> newList = new List<Artist> { newArtist1, newArtist2 };
+
+            //Act
+            List<Artist> result = Artist.GetAll();
+
+            //Assert
+            CollectionAssert.AreEqual(newList, result);
+        }
+
+        [TestMethod]
+        public void Find_ReturnsCorrectArtist_Artist()
+        {
+            //Arrange
+            string name01 = "Bon Iver";
+            string name02 = "Bruno Mars";
+            Artist newArtist1 = new Artist(name01);
+            Artist newArtist2 = new Artist(name02);
+
+            //Act
+            Artist result = Artist.FindById(2);
+
+            //Assert
+            Assert.AreEqual(newArtist2, result);
+        }
     }
 }
