5732832fc31a917341121b63ed91c53c5ace97d3
Krista Rutz <rutzkri000@gmail.com>
2020-03-12 10:24:50 -0700

    pass all tests for Record class Co-authored-by: Steven Fleming <12flemings@gmail.com> Co-authored-by: Krista Rutz <rutzkri000@gmail.com> Co-authored-by: Jack Dunning <JackStunning9001@gmail.com>

--- MusicOrganizer.Tests/ModelTests/RecordTests.cs
+++ MusicOrganizer.Tests/ModelTests/RecordTests.cs
@@ -61,5 +61,53 @@ namespace MusicOrganizer.Tests
             // Assert
             CollectionAssert.AreEqual(newList, result);
         }
+
+        [TestMethod]
+        public void GetAll_ReturnsRecord_RecordList()
+        {
+            //Arrange
+            string recordName01 = "Revolver";
+            string recordName02 = "Helplessness Blues";
+            Record newRecord1 = new Record(recordName01);
+            Record newRecord2 = new Record(recordName02);
+            List<Record> newList = new List<Record> { newRecord1, newRecord2 };
+
+            //Act
+            List<Record> result = Record.GetAll();
+
+            //Assert
+            CollectionAssert.AreEqual(newList, result);
+        }
+
+        [TestMethod]
+        public void GetId_RecordInstantiateWithAnIdAndGetterReturns_Int()
+        {
+            //Arrange
+            string recordName = "Revolver";
+            Record newRecord = new Record(recordName);
+
+            //Act
+            int result = newRecord.Id;
+
+            //Assert
+            Assert.AreEqual(1, result);
+        }
+        [TestMethod]
+        public void Find_ReturnsCorrectRecord_Record()
+        {
+            //Arrange
+            string recordName01 = "Revolver";
+            string recordName02 = "Helplessness Blues";
+            Record newRecord1 = new Record(recordName01);
+            Record newRecord2 = new Record(recordName02);
+
+            //Act
+            Record result = Record.Find(2);
+
+            //Assert
+            Assert.AreEqual(newRecord2, result);
+        }
+
+
     }
 }
