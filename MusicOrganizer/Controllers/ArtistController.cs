802a903e44e3b2447720ad08a7577842c861d29d
Krista Rutz <rutzkri000@gmail.com>
2020-03-12 10:52:51 -0700

    Add boilerplate for artist controller
    Co-authored-by: Steven Fleming <12flemings@gmail.com>
    Co-authored-by: Krista Rutz <rutzkri000@gmail.com>
    Co-authored-by: Jack Dunning <JackStunning9001@gmail.com>

---
+++ MusicOrganizer/Controllers/ArtistController.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using MusicOrganizer.Models;
+
+namespace MusicOrganizer.Controllers
+{
+  public class ArtistController : Controller
+  {
+
+  }
+}
