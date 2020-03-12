e3677ec8bad665758881e2f36c5d258624a034b1
Krista Rutz <rutzkri000@gmail.com>
2020-03-12 10:06:02 -0700

    "adds testing logic for List" Co-authored-by: Steven Fleming <12flemings@gmail.com> Co-authored-by: Krista Rutz <rutzkri000@gmail.com> Co-authored-by: Jack Dunning <JackStunning9001@gmail.com>

--- MusicOrganizer.Tests/ModelTests/RecordTests.cs
+++ MusicOrganizer.Tests/ModelTests/RecordTests.cs
@@ -1,10 +1,12 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MusicOrganizer.Models;
+using System.Collections.Generic;
+using System;
 
 namespace MusicOrganizer.Tests
 {
     [TestClass]
-    public class RecordTests
+    public class RecordTests : IDisposable
     {
         public void Dispose()
         {
@@ -29,6 +31,35 @@ namespace MusicOrganizer.Tests
 
             //Assert
             Assert.AreEqual(name, result);
+        }
+
+        [TestMethod]
+        public void UpdateName_UpdatesName_String()
+        {
+            //Arrange
+            string name = "Revolver";
+
+            //Act
+            string newRecordName = "Helplessness Blues";
+            Record newRecord = new Record(name);
+            newRecord.Name = newRecordName;
+            string result = newRecord.Name;
+
+            //Assert
+            Assert.AreEqual(newRecordName, result);
+        }
+
+        [TestMethod]
+        public void GetAll_ReturnsEmptyList_RecordList()
+        {
+            // Arrange
+            List<Record> newList = new List<Record> { };
+
+            // Act
+            List<Record> result = Record.GetAll();
+
+            // Assert
+            CollectionAssert.AreEqual(newList, result);
         }
     }
 }
